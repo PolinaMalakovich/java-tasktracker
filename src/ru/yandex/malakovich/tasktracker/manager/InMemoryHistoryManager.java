b6e6beeabc64cf4ec0d5081f4ce4a60c632cfed4
Polina Malakovich <polinamalakovich@gmail.com>
2022-03-30 17:25:54 +0200

    refactor: создать класс InMemoryHistoryManager

---
+++ src/ru/yandex/malakovich/tasktracker/manager/InMemoryHistoryManager.java
@@ -0,0 +1,17 @@
+package ru.yandex.malakovich.tasktracker.manager;
+
+import ru.yandex.malakovich.tasktracker.model.Task;
+
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    @Override
+    public void add(Task task) {
+
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return null;
+    }
+}
