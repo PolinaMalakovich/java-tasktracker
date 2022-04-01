fb5a5dab1f2f81a7d445629e2e8ddbae9683c37b
Polina Malakovich <polinamalakovich@gmail.com>
2022-04-01 09:12:39 +0200

    fix: добавить private конструктор утилитарному классу

--- src/ru/yandex/malakovich/tasktracker/manager/Managers.java
+++ src/ru/yandex/malakovich/tasktracker/manager/Managers.java
@@ -1,6 +1,8 @@
 package ru.yandex.malakovich.tasktracker.manager;
 
 public class Managers {
+    private Managers() {}
+
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
