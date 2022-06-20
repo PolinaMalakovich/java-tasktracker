cd8589d1c2cdf0bbc14c36220cb1449b4c976a54
Polina Malakovich <polinamalakovich@gmail.com>
2022-06-20 14:25:08 +0200

    refactor: добавить отсортированный список

--- src/main/java/ru/yandex/malakovich/tasktracker/manager/HttpTaskManager.java
+++ src/main/java/ru/yandex/malakovich/tasktracker/manager/HttpTaskManager.java
@@ -9,6 +9,7 @@ import ru.yandex.malakovich.tasktracker.model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class HttpTaskManager extends FileBackedTaskManager {
     private final KVTaskClient kvClient;
@@ -24,21 +25,23 @@ public class HttpTaskManager extends FileBackedTaskManager {
         if (needLoad) load();
     }
 
-    // TODO: 20.06.2022 добавить в HttpTaskManager обработку prioritizedTasks
     public void load() {
         String tasksList = kvClient.load("tasks");
         String subtasksList = kvClient.load("subtasks");
         String epicsList = kvClient.load("epics");
         String historyList = kvClient.load("history");
+        String prioritizedList = kvClient.load("prioritized");
         List<Task> tasks = gson.fromJson(tasksList, new TypeToken<ArrayList<Task>>(){}.getType());
         List<Subtask> subtasks = gson.fromJson(subtasksList, new TypeToken<ArrayList<Subtask>>(){}.getType());
         List<Epic> epics = gson.fromJson(epicsList, new TypeToken<ArrayList<Epic>>(){}.getType());
         List<Task> history = gson.fromJson(historyList, new TypeToken<ArrayList<Task>>(){}.getType());
+        Set<Task> prioritized = gson.fromJson(prioritizedList, new TypeToken<ArrayList<Task>>(){}.getType());
 
         tasks.forEach(task -> this.tasks.put(task.getId(), task));
         subtasks.forEach(subtask -> this.subtasks.put(subtask.getId(), subtask));
         epics.forEach(epic -> this.epics.put(epic.getId(), epic));
         history.forEach(this.historyManager::add);
+        this.prioritizedTasks.addAll(prioritized);
     }
 
     @Override
@@ -47,13 +50,16 @@ public class HttpTaskManager extends FileBackedTaskManager {
         List<Subtask> subtasks = getSubtasks();
         List<Epic> epics = getEpics();
         List<Task> history = history();
+        List<Task> prioritized = getPrioritizedTasksList();
         String tasksList = gson.toJson(tasks);
         String subtasksList = gson.toJson(subtasks);
         String epicsList = gson.toJson(epics);
         String historyList = gson.toJson(history);
+        String prioritizedList = gson.toJson(prioritized);
         kvClient.put("tasks", tasksList);
         kvClient.put("subtasks", subtasksList);
         kvClient.put("epics", epicsList);
         kvClient.put("history", historyList);
+        kvClient.put("prioritized", prioritizedList);
     }
 }
