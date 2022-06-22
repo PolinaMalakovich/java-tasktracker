00f70645dc773b266539ac44e32334e377145f1d
Polina Malakovich <polinamalakovich@gmail.com>
2022-06-22 20:24:23 +0200

    refactor: изменить логику загрузки и сохранения истории

--- src/main/java/ru/yandex/malakovich/tasktracker/manager/HttpTaskManager.java
+++ src/main/java/ru/yandex/malakovich/tasktracker/manager/HttpTaskManager.java
@@ -9,7 +9,11 @@ import ru.yandex.malakovich.tasktracker.model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HttpTaskManager extends FileBackedTaskManager {
     private final KVTaskClient kvClient;
@@ -29,18 +33,23 @@ public class HttpTaskManager extends FileBackedTaskManager {
         String tasksList = kvClient.load("tasks");
         String subtasksList = kvClient.load("subtasks");
         String epicsList = kvClient.load("epics");
-        String historyList = kvClient.load("history");
         String prioritizedList = kvClient.load("prioritized");
+        String historyList = kvClient.load("history");
+
         List<Task> tasks = gson.fromJson(tasksList, new TypeToken<ArrayList<Task>>(){}.getType());
         List<Subtask> subtasks = gson.fromJson(subtasksList, new TypeToken<ArrayList<Subtask>>(){}.getType());
         List<Epic> epics = gson.fromJson(epicsList, new TypeToken<ArrayList<Epic>>(){}.getType());
-        List<Task> history = gson.fromJson(historyList, new TypeToken<ArrayList<Task>>(){}.getType());
         Set<Task> prioritized = gson.fromJson(prioritizedList, new TypeToken<ArrayList<Task>>(){}.getType());
+
+        Map<Integer, Task> all = Stream
+                .concat(tasks.stream(), Stream.concat(subtasks.stream(), epics.stream()))
+                .collect(Collectors.toMap(Task::getId, Function.identity()));
+        List<Integer> history = gson.fromJson(historyList, new TypeToken<ArrayList<Integer>>(){}.getType());
+        history.stream().map(all::get).forEach(this.historyManager::add);
 
         tasks.forEach(task -> this.tasks.put(task.getId(), task));
         subtasks.forEach(subtask -> this.subtasks.put(subtask.getId(), subtask));
         epics.forEach(epic -> this.epics.put(epic.getId(), epic));
-        history.forEach(this.historyManager::add);
         this.prioritizedTasks.addAll(prioritized);
     }
 
@@ -49,7 +58,7 @@ public class HttpTaskManager extends FileBackedTaskManager {
         List<Task> tasks = getTasks();
         List<Subtask> subtasks = getSubtasks();
         List<Epic> epics = getEpics();
-        List<Task> history = history();
+        List<Integer> history = history().stream().map(Task::getId).collect(Collectors.toList());
         List<Task> prioritized = getPrioritizedTasksList();
         String tasksList = gson.toJson(tasks);
         String subtasksList = gson.toJson(subtasks);
